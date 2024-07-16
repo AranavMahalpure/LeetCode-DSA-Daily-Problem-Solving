d4000b4bd6972e516d526ca1a78690fb97bd8b3b
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-16 20:26:04 +0530

    Time: 5 ms (34.65%), Space: 8.3 MB (5.05%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long mySqrt(int x) {
+        int i=0 ,j=x;
+        long ans=0;
+        long mid;
+        while(i<=j){
+            mid=i+(j-i)/2;
+            if(mid*mid==x)
+            return mid;
+            else if (mid*mid<x){
+                ans=mid;
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
+
+            }
+            return ans;
+    }
+};
