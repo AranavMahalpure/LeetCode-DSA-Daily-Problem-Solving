13ef7a2fea314e2146d2995f1b1c9e99f5cf69f7
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-20 15:43:21 +0530

    Time: 23 ms (35.99%), Space: 27.4 MB (46.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,15 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         if(n==0) return ;
-        k = k % n; // Ensure k is within the range [0, n]
-
-        // Reverse the entire array
+        k = k % n;
         reverse(nums.begin(), nums.end());
-        
-        // Reverse the first k elements
         reverse(nums.begin(), nums.begin() + k);
-        
-        // Reverse the rest of the elements after k
         reverse(nums.begin() + k, nums.end());
     }
 };
