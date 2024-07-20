5a4d032e12dfdee2fa7a1cb6cc51d5d5fe30e159
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-20 23:19:53 +0530

    Time: 148 ms (80.74%), Space: 105.8 MB (14.61%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int maxFrequency(std::vector<int>& nums, int k) {
-    int cnt = 1;
-    int maxcnt = 1;
+    std::sort(nums.begin(), nums.end());
     long long sum = 0;
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
-    
+    int maxcnt = 1;
     int left = 0;
     
-    for (int right = 0; right < n; ++right) {
+    for (int right = 0; right < nums.size(); ++right) {
         sum += nums[right];
         
-        // Check if current window is valid
         while ((long long)(right - left + 1) * nums[right] - sum > k) {
             sum -= nums[left];
             ++left;
         }
         
-        // Update maxcnt if the current window size is greater
         maxcnt = std::max(maxcnt, right - left + 1);
     }
     
