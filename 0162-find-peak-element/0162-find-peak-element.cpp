eb336e5a7fd976f1f725b4b845a73d8bafc65573
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-21 10:19:07 +0530

    Time: 6 ms (31.24%), Space: 11.4 MB (13.14%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,16 +8,12 @@ public:
         int mid = left + (right - left) / 2;
 
         if (arr[mid] > arr[mid + 1]) {
-            // You are in the decreasing part of the mountain
-            // This means the peak is to the left (including mid)
             right = mid;
         } else {
-            // You are in the increasing part of the mountain
+
             left = mid + 1;
         }
     }
-
-    // When the while loop ends, left == right, pointing to the peak index
     return left;
     }
 };
