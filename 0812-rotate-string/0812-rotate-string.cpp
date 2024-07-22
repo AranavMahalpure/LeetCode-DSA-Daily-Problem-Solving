cb502c0bf2dcf48790511f5020b65b0cda74ad60
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-22 23:14:03 +0530

    Time: 0 ms (100%), Space: 8.1 MB (21.14%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string rotateclockwise(string s){
+        char c=s[s.size()-1];
+        int i=s.size()-2;
+        while(i>=0){
+            s[i+1]=s[i];
+            i--;
+        }
+        s[0]=c;
+        return s;
+    }
+    bool rotateString(string str1, string str2)
+    {
+        // Your code here
+        if(str1.size()!=str2.size()){
+            return 0;
+        }
+        string clockwise,anticlockwise;
+        int j=0;
+        clockwise=str1;
+        while(1 && j<str1.size()){
+        clockwise=rotateclockwise(clockwise);
+        if(clockwise==str2){
+            return 1;
+        }
+        j++;
+        }
+        return 0;
+    }
+};
