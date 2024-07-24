f555daff3b7550fd40676bd2d70210cc2fb31804
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-24 16:12:11 +0530

    Time: 8 ms (22.72%), Space: 10.4 MB (40.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,19 +1,62 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      int n=s.length(),i=n-1,j=n-1;
-    string ans;
-    while(i>=0)
-    {
-        if(s[i]==' ')
-        {
-            if(i!=j) ans=ans+s.substr(i+1,j-i)+' ';
-            i--; j=i;
+          int n=s.length();
+      string ans;
+      string result;
+      char c;
+      int i=s.size()-1;
+      while(i>=0){
+        if(s[i]!=' '){c=s[i];
+        ans+=c;
         }
-        else i--;
-    }
-    if(i!=j) ans=ans+s.substr(i+1,j-i);
-    else if(ans.back()==' ') ans.pop_back();
-    return ans;  
+
+        if(s[i]==' '){
+            reverse(ans.begin(),ans.end());
+            result+=ans+" ";
+            ans="";
+    
+        }
+        if(i==0){
+            reverse(ans.begin(),ans.end());
+            result+=ans+" ";
+            break;
+        }
+         i--;
+      }
+      while (!result.empty() && result.back() == ' ') {
+            result.pop_back();
+        }
+        while (!result.empty() && result.front() == ' ') {
+            result.erase(result.begin());
+        }
+        
+     // Remove extra spaces from the result
+        int j = 0;
+        while (result[j] == ' ') {
+            j++;
+        }
+        result = result.substr(j);
+
+        j = result.size() - 1;
+        while (result[j] == ' ') {
+            j--;
+        }
+        result = result.substr(0, j + 1);
+
+        // Remove extra spaces between words
+        string finalResult;
+        bool inSpace = false;
+        for (char ch : result) {
+            if (ch != ' ') {
+                finalResult += ch;
+                inSpace = false;
+            } else if (!inSpace) {
+                finalResult += ' ';
+                inSpace = true;
+            }
+        }
+
+        return finalResult;
     }
 };
