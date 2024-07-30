77437b26e5f65d18832f3cb743a0d5903faafd69
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-30 09:24:27 +0530

    Time: 25 ms (15.34%), Space: 24 MB (5.24%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-         int n =nums.size();
-         unordered_map<int,int> hmap;
-         for( int i=0;i<n;i++){
-            hmap[nums[i]]++;
-         }
-         for(auto it:hmap){
-            if(it.second<=1){
-                return it.first;
-            }
-         }
-         return -1;
+         unordered_map<int,int> a;
+	   for(auto x: nums)
+		   a[x]++;
+	   for(auto z:a)
+		   if(z.second==1)
+			   return z.first;
+	   return -1;
 
     }
 };
