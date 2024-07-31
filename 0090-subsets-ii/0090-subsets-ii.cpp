8af2571341c19ef0144fd83a16f0ff5c42a6fe63
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-31 18:55:22 +0530

    Time: 0 ms (100%), Space: 9.4 MB (41.55%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void fuct(int ind, int n, set<vector<int>>& ans, vector<int>& current, vector<int>& v) {
+        if (ind == n) {
+            ans.insert(current);
+            return;
+        }
+        // Include the element at index 'ind'
+        current.push_back(v[ind]);
+        fuct(ind + 1, n, ans, current, v);
+        // Exclude the element at index 'ind'
+        current.pop_back();
+        fuct(ind + 1, n, ans, current, v);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>> ans_set;
+        vector<int> current;
+        sort(nums.begin(), nums.end()); // Sort to handle duplicates
+        fuct(0, nums.size(), ans_set, current, nums);
+        
+        // Convert set to vector
+        vector<vector<int>> ans(ans_set.begin(), ans_set.end());
+        return ans;
+    }
+};
