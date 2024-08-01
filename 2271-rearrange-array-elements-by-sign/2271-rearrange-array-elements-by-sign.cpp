507a70b8c1ca035fb373b108f5268bf386a2c5d7
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-01 15:47:21 +0530

    Time: 138 ms (72.51%), Space: 126.6 MB (58.44%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-        int nextPositiveId(int i, vector<int> &nums) {
-    while (i < nums.size()) {
-      if (nums[i] > 0) {
-        return i;
-      }
-      i++;
+    vector<int> rearrangeArray(vector<int>& nums) {
+         int n = nums.size();
+        vector<int>ans(n,0);
+        int PositiveIndex=0;
+        int NegativeIndex=1;
+        for (int i=0;i<n;i++) {
+            if (nums[i]<0) {
+                ans[NegativeIndex]=nums[i];
+                NegativeIndex+=2;
+
+            }
+            else {
+                ans[PositiveIndex]=nums[i];
+                PositiveIndex+=2;
+            }
+
+        }
+        return ans;
     }
-    return i;
-  }
-
-  int nextnegativeId(int i, vector<int> &nums) {
-    while (i < nums.size()) {
-      if (nums[i] < 0) {
-        return i;
-      }
-      i++;
-    }
-    return i;
-  }
-
-public:
-  vector<int> rearrangeArray(vector<int> &nums) {
-    vector<int> ans;
-    int i = 0, j = 0, n = nums.size();
-    while (i < n && j < n) {
-      i = nextPositiveId(i, nums);
-      j = nextnegativeId(j, nums);
-      ans.push_back(nums[i++]);
-      ans.push_back(nums[j++]);
-    }
-
-    return ans;
-  }
-
 };
