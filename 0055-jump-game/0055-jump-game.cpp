816e589e036ca2c4a2c57573b357c65d0afb919d
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-03 10:31:14 +0530

    Time: 46 ms (50.42%), Space: 51.2 MB (28.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,8 @@ public:
         int reach = 0;
         for(int i=0;i<=reach;i++){
             reach = max(reach,i+nums[i]);
-            if(reach >= nums.size()-1) return true;
+            if(reach >= nums.size()-1)
+            { return true;}
         }
         return false;
     }
