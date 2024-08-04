41821b7315f9278885c755490e2c19fc3218d913
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-04 23:34:04 +0530

    Time: 15 ms (52.54%), Space: 19.5 MB (49.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-         unordered_map<int,int> a;
+        if(nums.size()==0){
+            return 0;
+        }
          int Xo=0;
 	   for(auto x: nums)
 		     Xo=Xo^x;
