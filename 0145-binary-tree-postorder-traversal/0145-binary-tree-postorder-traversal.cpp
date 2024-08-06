519d69fbac05411ae6f78427a38a68f023c5af59
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-06 11:03:03 +0530

    Time: 4 ms (31.65%), Space: 10.1 MB (36.78%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,17 +11,43 @@
  */
 class Solution {
 public:
-  void traveln(TreeNode* Node,vector<int>&ans){
-    if(Node==nullptr){
-        return;
-    }
-    traveln(Node->left,ans);
-    traveln(Node->right,ans);
-    ans.push_back(Node->val);
-  }
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        traveln(root,ans);
-        return ans;
-    }
+//   void traveln(TreeNode* Node,vector<int>&ans){
+//     if(Node==nullptr){
+//         return;
+//     }
+//     traveln(Node->left,ans);
+//     traveln(Node->right,ans);
+//     ans.push_back(Node->val);
+//   }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         traveln(root,ans);
+//         return ans;
+//     }
+ vector<int> postorderTraversal(TreeNode* root) {
+  vector<int> result;
+        if (root==NULL) return result;
+        stack<TreeNode*> stack;
+        TreeNode* lastVisited = NULL;
+        while (!stack.empty() || root != NULL){
+            if (root != NULL) {
+                stack.push(root);
+                root = root->left;
+            } else {
+                TreeNode* peekNode = stack.top();// current top node 
+                // If right child exists and traversing node from left child, then move right
+                if (peekNode->right != NULL && lastVisited != peekNode->right){
+                    root = peekNode->right;
+                }
+                // (lastvisited = NULL , Peek->right = NULL , so find the last root so pushing into result ) 
+                else{
+                    stack.pop();
+                    result.push_back(peekNode->val);
+                    lastVisited = peekNode;
+                }
+            }
+        }
+
+        return result;
+ }
 };
