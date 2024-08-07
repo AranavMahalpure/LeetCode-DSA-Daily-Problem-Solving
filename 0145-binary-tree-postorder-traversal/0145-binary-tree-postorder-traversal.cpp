74576f01a2787fa9546ebc756e27a3fde1727737
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-07 10:41:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (26.92%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -24,30 +24,56 @@ public:
 //         traveln(root,ans);
 //         return ans;
 //     }
- vector<int> postorderTraversal(TreeNode* root) {
-  vector<int> result;
-        if (root==NULL) return result;
-        stack<TreeNode*> stack;
-        TreeNode* lastVisited = NULL;
-        while (!stack.empty() || root != NULL){
-            if (root != NULL) {
-                stack.push(root);
-                root = root->left;
-            } else {
-                TreeNode* peekNode = stack.top();// current top node 
-                // If right child exists and traversing node from left child, then move right
-                if (peekNode->right != NULL && lastVisited != peekNode->right){
-                    root = peekNode->right;
-                }
-                // (lastvisited = NULL , Peek->right = NULL , so find the last root so pushing into result ) 
-                else{
-                    stack.pop();
-                    result.push_back(peekNode->val);
-                    lastVisited = peekNode;
-                }
-            }
-        }
+//using 1 stack
+//  vector<int> postorderTraversal(TreeNode* root) {
+//   vector<int> result;
+//         if (root==NULL) return result;
+//         stack<TreeNode*> stack;
+//         TreeNode* lastVisited = NULL;
+//         while (!stack.empty() || root != NULL){
+//             if (root != NULL) {
+//                 stack.push(root);
+//                 root = root->left;
+//             } else {
+//                 TreeNode* peekNode = stack.top();// current top node 
+//                 // If right child exists and traversing node from left child, then move right
+//                 if (peekNode->right != NULL && lastVisited != peekNode->right){
+//                     root = peekNode->right;
+//                 }
+//                 // (lastvisited = NULL , Peek->right = NULL , so find the last root so pushing into result ) 
+//                 else{
+//                     stack.pop();
+//                     result.push_back(peekNode->val);
+//                     lastVisited = peekNode;
+//                 }
+//             }
+//         }
 
-        return result;
- }
+//         return result;
+//  }
+ //using 2 stack
+  vector<int> postorderTraversal(TreeNode* root){
+    vector<int>post;
+    if(root==nullptr){
+        return post;
+    }
+    stack<TreeNode*>st1,st2;
+    st1.push(root);
+    while(!st1.empty()){
+        TreeNode * temp=st1.top();
+        st1.pop();
+        st2.push(temp);
+        if(temp->left){
+            st1.push(temp->left);
+    }
+    if(temp->right){
+        st1.push(temp->right);
+    }
+    }
+    while(!st2.empty()){
+        post.push_back(st2.top()->val);
+        st2.pop();
+    }
+    return post;
+  }
 };
