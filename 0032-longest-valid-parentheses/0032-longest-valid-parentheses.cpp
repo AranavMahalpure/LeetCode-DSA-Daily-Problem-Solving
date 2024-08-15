ada7f2c2899744ce61256a137184f3cb12bf1773
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-15 22:42:26 +0530

    Time: 7 ms (24.64%), Space: 8.9 MB (50.95%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+    stack<int> st;
+    int maxlen = 0;
+    
+    st.push(-1); 
+    
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            st.push(i);
+        } else {
+            st.pop();
+            if (st.empty()) {
+                st.push(i);
+            } else {
+                maxlen = max(maxlen, i - st.top());
+            }
+        }
+    }
+    
+    return maxlen;
+}
+};
