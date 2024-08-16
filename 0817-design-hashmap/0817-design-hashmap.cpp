875f1535b790249cd5a64e46bed2bb8e811f218f
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-17 01:02:49 +0530

    Time: 92 ms (86.23%), Space: 57.4 MB (80.66%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.cpp
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -1,22 +1,25 @@
 class MyHashMap {
 public:
-    vector<int>mp;
-    int size=1e6+1;
-    MyHashMap() {
-      mp.resize(size);
-      fill(mp.begin(),mp.end(),-1);
-    }
-    
+   std::unordered_map<int, int> mp;
+
+    MyHashMap() {}
+
+    // Store or update the value associated with the key
     void put(int key, int value) {
-       mp[key]=value;
+        mp[key] = value;  // Store the value directly
     }
     
+    // Retrieve the value for the key, or return -1 if the key doesn't exist
     int get(int key) {
-        return mp[key];
+        if (mp.find(key) != mp.end()) {
+            return mp[key];  // Return the value directly
+        }
+        return -1;  // Return -1 if the key is not found
     }
     
+    // Remove the key-value pair if it exists
     void remove(int key) {
-        mp[key]=-1;
+        mp.erase(key);  // Directly erase the key
     }
 };
 
