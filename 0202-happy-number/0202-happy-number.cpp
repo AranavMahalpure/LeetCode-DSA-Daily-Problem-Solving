b51e541abf0771289871f6b5f48e5ebf5b9ccc9b
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-09-07 12:49:49 +0530

    Time: 4 ms (28.97%), Space: 7.4 MB (87.43%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+      
+        int slow = n;
+        int fast = n;
+        do {
+            slow = square(slow);
+            fast = square(square(fast));
+        } while (slow != fast);
+
+//if a cycle exists, then the number is not a happy number
+//and slow will have a value other than 1
+
+        return slow == 1;
+    }
+    
+//Finding the square of the digits of a number
+
+int square(int num) {
+        
+        int ans = 0;
+        
+        while(num > 0) {
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num /= 10;
+        }
+        
+        return ans;
+    }
+};
